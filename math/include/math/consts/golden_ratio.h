29a251ff5b440c7102d92acbf672ccf492d48f5c
Ahmed Kerimov <kerimov.dev@yandex.ru>
2024-05-02 03:52:09 +0300

    Move consts

---
+++ math/include/math/consts/golden_ratio.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <numbers>
+
+#include "base/config/float.h"
+
+namespace consts {
+
+inline constexpr auto kPhi = std::numbers::phi_v<Float>;
+
+}  // namespace consts
---
+++ math/include/math/consts/pi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <numbers>
+
+#include "base/config/float.h"
+
+namespace consts {
+
+inline constexpr auto kPi = std::numbers::pi_v<Float>;
+
+}  // namespace consts
