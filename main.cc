91ee1a3eca84e048e4ca15ade7f1348c015f90e5
Ahmed Kerimov <kerimov.dev@yandex.ru>
2024-05-02 05:23:56 +0300

    Planck's law

--- main.cc
+++ main.cc
@@ -4,7 +4,8 @@
 #include "math/linalg/vector.h"
 #include "math/linalg/vector_io.h"
 #include "physics/consts/planck_constant.h"
+#include "physics/plancks_law.h"
 
 int main() {
-  std::cout << Vec3{1, Cube(2), consts::kPlanckConstant}.Normalized() << '\n';
+  std::cout << Vec3{func::UNu(1E15, 10000), Cube(2), consts::kPlanckConstant}.Normalized() << '\n';
 }
