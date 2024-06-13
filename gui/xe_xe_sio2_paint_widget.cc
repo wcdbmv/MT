4cd2a2d8e420f91be703ff904968f95fceeaae3b
Ahmed Kerimov <kerimov.dev@yandex.ru>
2024-06-14 01:05:22 +0300

    Paint XeXeSiO2PaintWidget

--- gui/xe_xe_sio2_paint_widget.cc
+++ gui/xe_xe_sio2_paint_widget.cc
@@ -41,63 +41,57 @@ void XeXeSiO2PaintWidget::paintEvent(QPaintEvent* /* event */) {
   const auto side_a = static_cast<int>(scale * a);
   const auto side_b = static_cast<int>(scale * b);
 
-  painter.setPen(QPen{Qt::black, 4});
-  painter.drawEllipse((width() - side_a) / 2, (height() - side_b) / 2, side_a,
-                      side_b);
-
   const auto r = win->ui->xexeSiO2RDoubleSpinBox->value();
   const auto delta = win->ui->xexeSiO2DeltaDoubleSpinBox->value();
   const auto n_plasma =
       static_cast<std::size_t>(win->ui->xexeSiO2NPlasmaSpinBox->value());
   const auto n_quartz =
       static_cast<std::size_t>(win->ui->xexeSiO2NQuartzSpinBox->value());
-  //
-  //    Float min = 9999999999.0_F;
-  //    Float max = -min;
-  //    if (win->xe_xe_sio2_res.has_value()) {
-  //      assert(win->xe_xe_sio2_res->absorbed_plasma.size() == n_plasma);
-  //      assert(win->xe_xe_sio2_res->absorbed_plasma3.size() == n_plasma);
-  //      assert(win->xe_xe_sio2_res->absorbed_quartz.size() == n_quartz + 1);
-  //      assert(win->xe_xe_sio2_res->absorbed_quartz3.size() == n_quartz + 1);
-  //      for (auto i3 : win->xe_xe_sio2_res->absorbed_plasma3) {
-  //        min = std::min(i3, min);
-  //        max = std::max(i3, max);
-  //      }
-  //      for (std::size_t i = 1; i <
-  //      win->xe_xe_sio2_res->absorbed_quartz3.size(); ++i) {
-  //        const auto i3 = win->xe_xe_sio2_res->absorbed_quartz3[i];
-  //        min = std::min(i3, min);
-  //        max = std::max(i3, max);
-  //      }
-  //    }
-  //
-  //  painter.setPen(QPen{Qt::black, 4});
-  //  if (win->xe_xe_sio2_res.has_value()) {
-  //      painter.setBrush(QBrush{QColor{
-  //          255,
-  //          static_cast<int>(
-  //              255 * (1.0 - (win->xe_xe_sio2_res->absorbed_quartz3.back() -
-  //              min) /
-  //                               (max - min))),
-  //          0}});
-  //  }
-  //
-  //  painter.drawEllipse((width() - side) / 2, (height() - side) / 2, side,
-  //  side);
-  //
+
+  Float min = 9999999999.0_F;
+  Float max = -min;
+  if (win->xe_xe_sio2_res.has_value()) {
+    assert(win->xe_xe_sio2_res->absorbed_plasma.size() == n_plasma);
+    assert(win->xe_xe_sio2_res->absorbed_plasma3.size() == n_plasma);
+    assert(win->xe_xe_sio2_res->absorbed_quartz.size() == n_quartz + 1);
+    assert(win->xe_xe_sio2_res->absorbed_quartz3.size() == n_quartz + 1);
+    for (auto i3 : win->xe_xe_sio2_res->absorbed_plasma3) {
+      min = std::min(i3, min);
+      max = std::max(i3, max);
+    }
+    for (std::size_t i = 1; i < win->xe_xe_sio2_res->absorbed_quartz3.size();
+         ++i) {
+      const auto i3 = win->xe_xe_sio2_res->absorbed_quartz3[i];
+      min = std::min(i3, min);
+      max = std::max(i3, max);
+    }
+  }
+
+  painter.setPen(QPen{Qt::black, 4});
+  if (win->xe_xe_sio2_res.has_value()) {
+    painter.setBrush(QBrush{QColor{
+        255,
+        static_cast<int>(
+            255 * (1.0 - (win->xe_xe_sio2_res->absorbed_quartz3.back() - min) /
+                             (max - min))),
+        0}});
+  }
+
+  painter.drawEllipse((width() - side_a) / 2, (height() - side_b) / 2, side_a,
+                      side_b);
+
   painter.setPen(QPen{Qt::black, 1});
   for (std::size_t i = n_quartz - 1; i > 0; --i) {
-    //          QBrush brush{};
-    //          if (win->xe_xe_sio2_res.has_value()) {
-    //            brush = QBrush{QColor{
-    //                255,
-    //                static_cast<int>(
-    //                    255 * (1.0 -
-    //                    ((win->xe_xe_sio2_res->absorbed_quartz3[i] - min) /
-    //                                  (max - min)))),
-    //                0}};
-    //          }
-    //          painter.setBrush(brush);
+    QBrush brush{};
+    if (win->xe_xe_sio2_res.has_value()) {
+      brush = QBrush{QColor{
+          255,
+          static_cast<int>(
+              255 * (1.0 - ((win->xe_xe_sio2_res->absorbed_quartz3[i] - min) /
+                            (max - min)))),
+          0}};
+    }
+    painter.setBrush(brush);
 
     const auto side_ai = static_cast<int>(scale * a * static_cast<Float>(i) /
                                           static_cast<Float>(n_quartz));
@@ -106,18 +100,17 @@ void XeXeSiO2PaintWidget::paintEvent(QPaintEvent* /* event */) {
     painter.drawEllipse((width() - side_ai) / 2, (height() - side_bi) / 2,
                         side_ai, side_bi);
   }
-  //
+
   painter.setPen(QPen{Qt::black, 4});
   painter.setBrush(QBrush{Qt::white});
-  //  if (win->xe_xe_sio2_res.has_value()) {
-  //    painter.setBrush(QBrush{QColor{
-  //        255,
-  //        static_cast<int>(
-  //            255 * (1.0 - (win->xe_xe_sio2_res->absorbed_plasma3.back() -
-  //            min) /
-  //                             (max - min))),
-  //        0}});
-  //  }
+  if (win->xe_xe_sio2_res.has_value()) {
+    painter.setBrush(QBrush{QColor{
+        255,
+        static_cast<int>(
+            255 * (1.0 - (win->xe_xe_sio2_res->absorbed_plasma3.back() - min) /
+                             (max - min))),
+        0}});
+  }
   const auto side_plasma = static_cast<int>(scale * r);
   const auto delta_scale = static_cast<int>(scale * delta);
   painter.drawEllipse((width() - delta_scale) / 2 - side_plasma,
@@ -127,18 +120,18 @@ void XeXeSiO2PaintWidget::paintEvent(QPaintEvent* /* event */) {
 
   painter.setPen(QPen{Qt::black, 1});
   for (std::size_t i = n_plasma - 1; i > 0; --i) {
-    //    QBrush brush{};
-    //    if (win->xe_xe_sio2_res.has_value()) {
-    //          brush = QBrush{QColor{
-    //              255,
-    //              static_cast<int>(
-    //                  255 * (1.0 - ((win->xe_xe_sio2_res->absorbed_plasma3[i -
-    //                  1] - min) /
-    //                                (max - min)))),
-    //              0}};
-    //    }
-    //    painter.setBrush(brush);
-    //
+    QBrush brush{};
+    if (win->xe_xe_sio2_res.has_value()) {
+      brush = QBrush{QColor{
+          255,
+          static_cast<int>(
+              255 *
+              (1.0 - ((win->xe_xe_sio2_res->absorbed_plasma3[i - 1] - min) /
+                      (max - min)))),
+          0}};
+    }
+    painter.setBrush(brush);
+
     const auto side_i = static_cast<int>(scale * r * static_cast<Float>(i) /
                                          static_cast<Float>(n_plasma));
     painter.drawEllipse((width() - delta_scale - side_plasma - side_i) / 2,
